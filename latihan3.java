565cdf5a27d9c4a75c1f48364ea8a4b24867635e
13020180060 <61998954+13020180060@users.noreply.github.com>
2020-03-10 16:33:32 +0800

    Add files via upload

---
+++ latihan3.java
@@ -0,0 +1,9 @@
+NAMA    : ITA MASITA
+TANGGAL : 09 MARET 2020
+WAKTU   : 16.30
+public class latihan3{
+	public static void main(String[] args)
+	{
+		System.out.print("Selesai belajar pemrograman java");
+	}
+}	
